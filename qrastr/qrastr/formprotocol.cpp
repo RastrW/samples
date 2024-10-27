460e93bec6e13b1f63cf4cc31a7e4eeb1e0a1305
AleksandrAleksandrov1980 <ustas_soft@mail.ru>
2024-10-27 11:33:29 +0500

    cleaned up

--- qrastr/qrastr/formprotocol.cpp
+++ qrastr/qrastr/formprotocol.cpp
@@ -12,16 +12,8 @@ FormProtocol::FormProtocol(QWidget *parent)
     :QWidget(parent)
     ,ui(new Ui::FormProtocol){
     ui->setupUi(this);
-    // определяем данные для модели
-    QStringList list = { "Tom", "Bob", "Sam" };
-    // определяем модель
-    pmodel_ = new QStringListModel(list);
-    //ui->twProtocol->setModel(pmodel_);
     p_protocol_tree_model_ = new ProtocolTreeModel();
     ui->twProtocol->setModel(p_protocol_tree_model_);
-    //new ProtocolTreeModel();
-    spti_stage_ = p_protocol_tree_model_->getRootItemSp();
-    spti_stage_previos_ = spti_stage_;
     s_spti_stages_.emplace( p_protocol_tree_model_->getRootItemSp() );
 }
 FormProtocol::~FormProtocol(){
@@ -30,125 +22,51 @@ FormProtocol::~FormProtocol(){
 //https://doc.qt.io/qt-5/qtwidgets-itemviews-simpletreemodel-example.html
 void FormProtocol::onAppendProtocol(const QString& qstr){
     auto sp_item = std::make_shared<ProtocolTreeItem>( QVariantList{
-        QString("row%1").arg(n_row_counter_), qstr
-        },s_spti_stages_.top().get() );
-        //}, spti_stage_previos_.get() );
-    //spti_stage_->appendChild(sp_item);
+        QString("protocol"), qstr
+        }, s_spti_stages_.top().get() );
     s_spti_stages_.top().get()->appendChild(sp_item);
     return;
-
-    //std::string str{qstr.toStdString()};
-    std::string str{""};
-    str += qstr.toStdString();
-    //encode(str);
-    str += "\n";
-    //shProt_->my_appendTect(str);
-    if(pmodel_->insertRow(pmodel_->rowCount())) {
-        QModelIndex index = pmodel_->index(pmodel_->rowCount() - 1, 0);
-        pmodel_->setData(index, qstr);
-    }else{
-        assert(!"cant insert row in Protocol");
-    }
-    //QVariant qv = qstr;
-    QVariantList qvl;
-    qvl<< qstr;
-    ProtocolTreeItem* pti_new = new ProtocolTreeItem(qvl);
-    //p_protocol_tree_model_->in
-
-    //if( p_protocol_tree_model_->insertRow( p_protocol_tree_model_->rowCount() ) ){
-    //    QModelIndex qindx = p_protocol_tree_model_->index( p_protocol_tree_model_->rowCount()-1,0);
-    //    p_protocol_tree_model_->setData(qindx,qstr);
-    //}
-    //p_protocol_tree_model_->getRootItem()->appendChild(std::make_unique<ProtocolTreeItem>(qvl));
-
-    for(int i = 0 ; i < 1 ; i++, n_row_counter_++){
-        //auto item = std::make_unique<ProtocolTreeItem>( QVariantList{tr(QString("row%1").arg(n_row_counter_).toUtf8().constData()), qstr.toUtf8().constData()}, p_protocol_tree_model_->getRootItem() );
-        auto item = std::make_shared<ProtocolTreeItem>( QVariantList{QString("row%1").arg(n_row_counter_), qstr}, p_protocol_tree_model_->getRootItem() );
-
-        ProtocolTreeItem* p_item_prev = item.get();
-        for(int j = 0 ; j < 10 ; j++){
-            QString qstr2 = qstr;
-            qstr2.insert(0,"itemm-2-");
-            //auto item2 = std::make_unique<ProtocolTreeItem>( QVariantList{tr(QString("item-2-row%1").arg(n_row_counter_).toUtf8().constData()), qstr2.toUtf8().constData()}, item.get() );
-            auto item2 = std::make_shared<ProtocolTreeItem>( QVariantList{QString("item-2-row%1").arg(n_row_counter_), qstr2}, item.get() );
-            p_item_prev = item2.get();
-            qstr2.insert(0,"itemm-3-");
-            //auto item3 = std::make_unique<ProtocolTreeItem>( QVariantList{tr(QString("item-3-row%1").arg(n_row_counter_).toUtf8().constData()), qstr2.toUtf8().constData()}, item2.get() );
-            auto item3 = std::make_shared<ProtocolTreeItem>( QVariantList{QString("item-3-row%1").arg(n_row_counter_), qstr2}, item2.get() );
-            item2->appendChild(item3);
-            item->appendChild((item2));
-        }
-        //item->appendChild();
-        p_protocol_tree_model_->getRootItem()->appendChild( item );
-        //p_protocol_tree_model_->getRootItem()->appendChild(std::make_unique<ProtocolTreeItem>( QVariantList{tr(QString("row%1").arg(n_row_counter_).toUtf8().constData()), qstr.toUtf8().constData()}, p_protocol_tree_model_->getRootItem() ));
-    }
-    //p_protocol_tree_model_->setData()
-    //p_protocol_tree_model_->se
 }
 void FormProtocol::onRastrLog(const _log_data& log_data){
     QString qstr2 ;
     p_protocol_tree_model_->layoutAboutToBeChanged();
-//https://forum.qt.io/topic/87721/index-for-begininsertrows-with-qtreeview/9
-//https://wiki.qt.io/Model_Test
+    //https://forum.qt.io/topic/87721/index-for-begininsertrows-with-qtreeview/9
+    //https://wiki.qt.io/Model_Test
     //beginInsertRows(parent, 2, 4);
     //endInsertRows();
- //   p_protocol_tree_model_->beginInsertRows(QModelIndex(),spti_stage_->childCount(), spti_stage_->childCount()+1);
-    //auto item = std::make_shared<ProtocolTreeItem>( QVariantList{tr(QString("row%1").arg(n_row_counter_).toUtf8().constData()), qstr.toUtf8().constData()}, p_protocol_tree_model_->getRootItem() );
-    std::string str = "";
     if( LogMessageTypes::OpenStage == log_data.lmt ){
-        str += "<STAGE";
-        str += std::to_string(log_data.n_stage_id);
-        str += ">\t";
-        str += log_data.str_msg;
-        str += "\n";
-
         auto sp_item = std::make_shared<ProtocolTreeItem>( QVariantList{
-            QString("STAGE!!%1").arg(n_row_counter_), log_data.str_msg.c_str()
-            //}, spti_stage_.get() );
+            QString("STAGE!!%1").arg(log_data.n_stage_id), log_data.str_msg.c_str()
             },s_spti_stages_.top().get() );
-        //spti_stage_->appendChild(sp_item);
-
-    //    spti_stage_previos_ = spti_stage_;
-     //   spti_stage_ = sp_item;
-
-        //spti_stage_previos_->appendChild(sp_item);
         s_spti_stages_.top()->appendChild(sp_item);
         s_spti_stages_.emplace(sp_item);
-        //spti_stage_ = std::make_shared<ProtocolTreeItem>( QVariantList{tr(QString("item-3-row%1").arg(n_row_counter_).toUtf8().constData()), qstr2.toUtf8().constData()}, item2.get() );
-        //shProt_->my_appendTect(str);
-        //n_stage_max_id_ = log_data.n_stage_id;
     }else if( LogMessageTypes::CloseStage == log_data.lmt ){
-        str += "</STAGE";
-        str += std::to_string(log_data.n_stage_id);
-        str += ">\n";
-
-
-   //     spti_stage_ = spti_stage_previos_;
-        s_spti_stages_.pop();
-
-        //shProt_->my_appendTect(str);
-        //assert(n_stage_max_id_ == log_data.n_stage_id);
-        //if(n_stage_max_id_ == log_data.n_stage_id){
-        //    n_stage_max_id_--;
-        //}
-    }else {
-
+        if(1 < s_spti_stages_.size()){
+            s_spti_stages_.pop();
+        }else{
+            assert(!"trying to close Root stage!");
+        }
+    }else{
+        QString qstr_type{};
+        switch(log_data.lmt){
+            case LogMessageTypes::SystemError:  qstr_type = "SYSTEM-ERROR"; break;
+            case LogMessageTypes::Failed:       qstr_type = "FAILED"; break;
+            case LogMessageTypes::Error:        qstr_type = "ERROR"; break;
+            case LogMessageTypes::Warning:      qstr_type = "Warning"; break;
+            case LogMessageTypes::Message:      qstr_type = "msg"; break;
+            case LogMessageTypes::Info:         qstr_type = "inf"; break;
+            case LogMessageTypes::EnterDefault: qstr_type = "EnterDefault"; break;
+            case LogMessageTypes::Reset:        qstr_type = "RESET!"; break;
+            case LogMessageTypes::None:         qstr_type = "None"; break;
+            case LogMessageTypes::OpenStage:
+            case LogMessageTypes::CloseStage:
+            default:                            qstr_type = "InnerFail"; break;
+        }
         auto sp_item = std::make_shared<ProtocolTreeItem>( QVariantList{
-            QString("msg!!%1").arg(n_row_counter_), log_data.str_msg.c_str()
-           // }, spti_stage_.get() );
-             },s_spti_stages_.top().get() );
-        //spti_stage_->appendChild(sp_item);
-
-        //spti_stage_previos_ = spti_stage_;
-        //spti_stage_ = sp_item;
-
-        //spti_stage_->appendChild(sp_item);
+            qstr_type, log_data.str_msg.c_str()
+            } , s_spti_stages_.top().get() );
         s_spti_stages_.top()->appendChild(sp_item);
-
     }
-//    p_protocol_tree_model_->endInsertRows();
-//    p_protocol_tree_model_->rowsInserted(QModelIndex{}, 0,0);
-     p_protocol_tree_model_->layoutChanged();
+    p_protocol_tree_model_->layoutChanged();
     new QAbstractItemModelTester( p_protocol_tree_model_, QAbstractItemModelTester::FailureReportingMode::Fatal, this);
-    //emit dataChanged();
 }
--- qrastr/qrastr/formprotocol.h
+++ qrastr/qrastr/formprotocol.h
@@ -23,12 +23,8 @@ private slots:
     void onAppendProtocol(const QString& qstr);
 private:
     Ui::FormProtocol*  ui = nullptr;
-    QStringListModel*  pmodel_ = nullptr;
     ProtocolTreeModel* p_protocol_tree_model_;
-    std::int64_t n_row_counter_ = 0;
-    std::shared_ptr<ProtocolTreeItem> spti_stage_previos_;
-    std::shared_ptr<ProtocolTreeItem> spti_stage_;
-    std::stack< std::shared_ptr<ProtocolTreeItem> > s_spti_stages_;
+    std::stack< std::shared_ptr< ProtocolTreeItem > > s_spti_stages_;
 };
 
 #endif // FORMPROTOCOL_H
