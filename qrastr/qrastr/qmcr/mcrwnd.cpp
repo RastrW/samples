8f7b60450071b1732a891427b90bd45f687ef650
ustas_a181 <ustas@niipt-ems.ru>
2025-11-01 12:35:45 +0500

    chng: macro

--- qrastr/qrastr/qmcr/mcrwnd.cpp
+++ qrastr/qrastr/qmcr/mcrwnd.cpp
@@ -98,6 +98,31 @@ for i in range(0, tabs.count-1):
     rastr.print(tabs[i])
     if (i == 6): break
 
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title("Widgets Demo")
+
+widgets = [
+    tk.Label,
+    tk.Checkbutton,
+    ttk.Combobox,
+    tk.Entry,
+    tk.Button,
+    tk.Radiobutton,
+    tk.Scale,
+    tk.Spinbox,
+]
+
+for widget in widgets:
+    try:
+        widget = widget(root, text=widget.__name__)
+    except tk.TclError:
+        widget = widget(root)
+    widget.pack(padx=5, pady=5, fill="x")
+
+root.mainloop()
 
 from turtle import Turtle
 from random import random
@@ -111,11 +136,13 @@ for i in range(30):
 t.screen.mainloop()
 
 
-from tkinter import *
-root = Tk()
-root.geometry('160x140+800+300')
-Label(text='Thats all folks', width=20, height=3).pack()
-root.mainloop()
+#from tkinter import *
+#root = Tk()
+#root.geometry('160x140+800+300')
+#Label(text='Thats all folks', width=20, height=3).pack()
+#root.mainloop()
+
+
 
 )"};
 
