c886cb700d0e764c79427437e6fc2a704de223cd
Dmitriy <maksimenko@niipt-ems.ru>
2026-02-26 10:22:33 +0500

    chng macro

--- qrastr/Data/contextmacro/sel_ostrov.py
+++ qrastr/Data/contextmacro/sel_ostrov.py
@@ -2,38 +2,32 @@
 
 rastr.print(f"Hello from contextmacro ! row = {aRow}")
 
-def VKL(i:int):
-    node=rastr.table("node")
-    vetv=rastr.table("vetv")
-    sel = node.column("sel")
-    ny = node.column("ny")
-    #sta = node.column("sta")
-    vet_ip = vetv.column("ip")
-    vet_iq = vetv.column("iq")
-    vet_sta = vetv.column("sta")
+def VKL(index: int) -> None:   
+    node = rastr["node"]
+    vetv = rastr["vetv"]
+    sel = node["sel"]
+    ny = node["ny"]
+    vet_ip = vetv["ip"]
+    vet_iq = vetv["iq"]
+    vet_sta = vetv["sta"]
     
-    n = ny[i]
-    #print(f"node = {n}")
-    sel[i] = True
-    vetv.set_selection(f"ip= {n} |iq= {n}")
+    n = ny[index]
+    sel[index] = True
+    vetv.set_selection(f"ip={n}|iq={n}")
     j = vetv.find_next(-1)
-    #print(f"j= {j}")
 
-    while j>-1:
+    while j != -1:
         st = vet_sta[j]
-        if st==0:
-            nf=vet_ip[j]
-           # print(f"nf= {nf}")
-            if nf==n: nf=vet_iq[j]
+        if st == 0:
+            nf = vet_ip[j]
+            if nf == n:
+                nf = vet_iq[j]
             node.set_selection(f"ny={nf}")
-            jn=node.find_next(-1)
-            #print(f"jn= {jn}")
+            jn = node.find_next(-1)
 
-            if jn>-1:
-                if sel[jn]==False:
-                    VKL(jn) 
-        #print(f"j= {j}")
-        j=vetv.find_next(j)
+            if jn != -1 and sel[jn] == False:
+                VKL(jn)
+        j = vetv.find_next(j)
 
 
 VKL(aRow)
